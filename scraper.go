d818b3794279fbe16647fc82f0120fea4cb5fdee
Joel Mäenpää <zoloppe4lyf@gmail.com>
2021-03-09 12:31:58 +0200

    comets

--- scraper.go
+++ scraper.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"encoding/csv"
 	"github.com/gocolly/colly"
 	structs "bossScraper/structs"
 )
 
 func main() {
+	// create a collector
 	c := colly.NewCollector(
 		colly.AllowedDomains("darksouls3.wiki.fextralife.com"),
 	)
 
+	// log some stuff when visiting url
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
+	// create a slice to store bosses
 	bosses := []structs.Boss{}
+
+	// search for tbody element
 	c.OnHTML("tbody", func(e *colly.HTMLElement) {
+		// loop through rows and create a boss for each
 		e.ForEach("tr", func(_ int, row *colly.HTMLElement) {
 			boss := structs.Boss {
 				Boss: row.ChildText("td:nth-child(1)"),
@@ -29,10 +37,12 @@ func main() {
 				Optional: row.ChildText("td:nth-child(8)"),
 			}
 
+			// append boss to bosses
 			bosses = append(bosses, boss)
 		})
 		fmt.Println(bosses)
 	})
 
+	// url to scrape
 	c.Visit("https://darksouls3.wiki.fextralife.com/Bosses")
 }
