e512853481296e3cd7cc8ef587c63857db1c380c
Joel Mäenpää <zoloppe4lyf@gmail.com>
2021-03-09 11:58:21 +0200

    struct in a different place

---
+++ structs/boss.go
@@ -0,0 +1,13 @@
+package structs
+
+// Boss structure
+type Boss struct {
+	Boss         string
+	Location     string
+	NPCSummoning string
+	Weakness     string
+	Resistance   string
+	Immunity     string
+	Parryable    string
+	Optional     string
+}
